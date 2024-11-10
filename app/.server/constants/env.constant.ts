8df8e780d35c3d85558cd6c411827abe11ca4da3
tuansdf <103062682+tuansdf@users.noreply.github.com>
2024-11-10 17:36:32 +0700

    update deps

--- app/.server/constants/env.constant.ts
+++ app/.server/constants/env.constant.ts
@@ -1,7 +1,7 @@
 // private env
 
 export const ENV = {
-  DB_URL: process.env.DB_URL,
-  SALT: process.env.SALT,
-  SUPER_PASSWORD: process.env.SUPER_PASSWORD,
+  DB_URL: process.env.DB_URL || "",
+  SALT: process.env.SALT || "",
+  SUPER_PASSWORD: process.env.SUPER_PASSWORD || "",
 };
--- app/.server/databases/db.ts
+++ app/.server/databases/db.ts
@@ -1,7 +1,8 @@
 import { ENV } from "@/.server/constants/env.constant";
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import { drizzle } from "drizzle-orm/libsql";
 
-export const connection = new Database(ENV.DB_URL);
-
-export const db = drizzle(connection);
+export const db = drizzle({
+  connection: {
+    url: ENV.DB_URL,
+  },
+});
