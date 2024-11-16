950f9ac441392c154b6464a11b51b6715185ab44
tuansdf <103062682+tuansdf@users.noreply.github.com>
2024-11-16 12:34:59 +0700

    add delete note

--- app/client/features/notes/note-history.tsx
+++ app/client/features/notes/note-history.tsx
@@ -1,10 +1,11 @@
 import { HistoryItem } from "@/client/features/vaults/history-item";
 import { useAppStore } from "@/client/stores/app.store";
 import { Alert, Box, Text } from "@mantine/core";
+import { modals } from "@mantine/modals";
 import { useMemo } from "react";
 
 export const NoteHistory = () => {
-  const { noteUrls } = useAppStore();
+  const { noteUrls, removeNoteUrl } = useAppStore();
 
   const noteUrlsArr = useMemo(() => {
     if (!noteUrls) return;
@@ -15,6 +16,18 @@ export const NoteHistory = () => {
     return <Alert color="blue" title="Nothing..." />;
   }
 
+  const handleDelete = (item: string) => {
+    modals.openConfirmModal({
+      title: "Delete note",
+      children: <Text size="sm">Are you sure you want to delete this note?</Text>,
+      labels: { confirm: "Confirm", cancel: "Cancel" },
+      confirmProps: { color: "red" },
+      onConfirm: () => {
+        removeNoteUrl(item);
+      },
+    });
+  };
+
   return (
     <Box className="k-history-container">
       {noteUrlsArr.map((item) => {
@@ -22,6 +35,7 @@ export const NoteHistory = () => {
         return (
           <HistoryItem
             key={item}
+            onDelete={() => handleDelete(item)}
             text={
               <Text component="a" target="_blank" href={item} truncate="end" c="blue">
                 {item}
