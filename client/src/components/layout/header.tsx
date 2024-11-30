5fa8c9825c59abb54b884f019b9c6635fdc6065c
tuansdf <103062682+tuansdf@users.noreply.github.com>
2024-12-01 02:30:12 +0700

    refactor(client): show note url on success

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -1,17 +1,22 @@
-import { A, useNavigate } from "@solidjs/router";
+import { A } from "@solidjs/router";
+import { Show } from "solid-js";
 
-export const Header = () => {
-  const navigate = useNavigate();
+type Props = {
+  onNewNote?: () => void;
+};
 
+export const Header = (props: Props) => {
   return (
     <header class="border-bottom">
-      <div class="d-flex justify-content-between align-items-center container-xxl py-2">
-        <A href="/" class="text-decoration-none text-light">
+      <div class="d-flex justify-content-between align-items-center container-xxl py-0" style={{ height: "3rem" }}>
+        <A href="/" class="text-decoration-none text-light" onClick={props.onNewNote}>
           <h1 class="fs-5 fw-semibold m-0">Pastebin</h1>
         </A>
-        <button class="btn btn-dark" onClick={() => navigate("/")}>
-          New note
-        </button>
+        <Show when={props.onNewNote}>
+          <button class="btn btn-dark" onClick={props.onNewNote}>
+            New note
+          </button>
+        </Show>
       </div>
     </header>
   );
--- client/src/pages/create-note-page.tsx
+++ client/src/pages/create-note-page.tsx
@@ -1,12 +1,135 @@
+import { createVault } from "@/api/vault.api.ts";
 import { Header } from "@/components/layout/header.tsx";
-import { CreateNoteForm } from "@/components/notes/create-note-form.tsx";
+import { Alert } from "@/components/ui/alert.tsx";
+import { Button } from "@/components/ui/button.tsx";
+import { ScreenLoading } from "@/components/ui/screen-loading.tsx";
+import { Textarea } from "@/components/ui/textarea.tsx";
+import { DEFAULT_ERROR_MESSAGE, MAX_CONTENT_LENGTH } from "@/contants/common.constant.ts";
+import { validateUrl } from "@/utils/common.util.ts";
+import { encryptText, generateEncryptionConfigs, generatePassword } from "@/utils/crypto.util.ts";
+import { A } from "@solidjs/router";
+import { createSignal, Match, Show, Switch } from "solid-js";
 
 export function CreateNotePage() {
+  const [content, setContent] = createSignal<string>("");
+  const [errors, setErrors] = createSignal<{ content?: string; common?: string }>({});
+  const [isLoading, setIsLoading] = createSignal<boolean>(false);
+  const [resultUrl, setResultUrl] = createSignal<string>("");
+
+  const resetForm = () => {
+    setContent("");
+    setErrors({});
+    setIsLoading(false);
+    setResultUrl("");
+  };
+
+  const handleNoteSubmit = async () => {
+    if (!content()) {
+      return setErrors({ content: "Required" });
+    }
+    if (content().length > MAX_CONTENT_LENGTH) {
+      return setErrors({ content: `Content must be less than ${MAX_CONTENT_LENGTH} characters` });
+    }
+    setErrors({});
+
+    try {
+      setIsLoading(true);
+      const encryptionConfigs = generateEncryptionConfigs();
+      const password = generatePassword();
+      const encrypted = await encryptText(content(), password, encryptionConfigs.nonce);
+      const data = await createVault({
+        content: encrypted,
+        configs: { encryption: encryptionConfigs },
+      });
+      if (!data) return setErrors({ common: DEFAULT_ERROR_MESSAGE });
+      const id = data.id;
+      setResultUrl(`${location.origin}/${id}#${password}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleUrlSubmit = async () => {
+    if (!validateUrl(content().trim())) {
+      return setErrors({ content: "URL is invalid" });
+    }
+    setErrors({});
+
+    try {
+      setIsLoading(true);
+      const encryptionConfigs = generateEncryptionConfigs();
+      const password = generatePassword();
+      const encrypted = await encryptText(content(), password, encryptionConfigs.nonce);
+      const data = await createVault({
+        content: encrypted,
+        configs: { encryption: encryptionConfigs },
+      });
+      if (!data) return setErrors({ common: DEFAULT_ERROR_MESSAGE });
+      const id = data.id;
+      setResultUrl(`${location.origin}/s/${id}#${password}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
-      <Header />
+      <Header onNewNote={resetForm} />
+
       <div class="container-xxl p-3">
-        <CreateNoteForm />
+        <Show when={errors().common}>
+          <Alert variant="danger">{errors().common}</Alert>
+        </Show>
+
+        <Switch>
+          <Match when={resultUrl()}>
+            <div class="">
+              <Alert>
+                Your note:{" "}
+                <A class="text-info" href={resultUrl()}>
+                  {resultUrl()}
+                </A>
+              </Alert>
+            </div>
+          </Match>
+          <Match when={true}>
+            <form>
+              <Textarea
+                class="font-monospace"
+                rows={20}
+                value={content()}
+                onInput={(e) => {
+                  setContent(e.currentTarget.value);
+                }}
+                minLength={1}
+                maxLength={MAX_CONTENT_LENGTH}
+                required
+                errorMessage={errors().content}
+                letterCount={content().length}
+                maxLetterCount={MAX_CONTENT_LENGTH}
+                placeholder="Write your notes here..."
+              />
+              <div class="d-flex justify-content-end mt-2 gap-2">
+                <Button type="button" variant="dark" onClick={handleUrlSubmit}>
+                  Create URL
+                </Button>
+                <Button
+                  type="submit"
+                  onClick={async (e) => {
+                    e.preventDefault();
+                    await handleNoteSubmit();
+                  }}
+                >
+                  Create note
+                </Button>
+              </div>
+            </form>
+          </Match>
+        </Switch>
+
+        <Show when={isLoading()}>
+          <ScreenLoading />
+        </Show>
       </div>
     </>
   );
