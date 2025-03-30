02de99c38d4301ed50c8c8f4f995ee657b3cc111
tuansdf <103062682+tuansdf@users.noreply.github.com>
2025-03-30 21:59:54 +0700

    chore(client): update build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,17 @@
+import viteLegacyPlugin from "@vitejs/plugin-legacy";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import solid from "vite-plugin-solid";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [solid(), tsconfigPaths(), VitePWA({ registerType: "autoUpdate", manifest: { theme_color: "#ffffff" } })],
+  plugins: [
+    solid(),
+    tsconfigPaths(),
+    viteLegacyPlugin(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: { name: "Pastebin", short_name: "Pastebin", theme_color: "#ffffff" },
+    }),
+  ],
 });
